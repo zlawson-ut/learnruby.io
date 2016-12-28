a41479875e7bd40b43cb45766c12ef9f75ca7770
Zach Lawson <zachlawson.dev@gmail.com>
2016-12-28 15:59:12 -0700

    Addd favicons

--- header.php
+++ header.php
@@ -21,22 +21,16 @@
 	<meta property="og:image" content="<?php echo 'siteImage' ?>" />
 	<meta property="og:description" content="<?php echo 'metaDescription' ?>" />
 
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon-57x57.png'; ?>">
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon-114x114.png'; ?>">
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon-72x72.png'; ?>">
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon-144x144.png'; ?>">
-	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon-60x60.png'; ?>">
-	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon-120x120.png'; ?>">
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon-76x76.png'; ?>">
-	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon-152x152.png'; ?>">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/favicons/favicon-196x196.png" sizes="196x196'; ?>">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/favicons/favicon-160x160.png" sizes="160x160'; ?>">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/favicons/favicon-96x96.png" sizes="96x96'; ?>">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/favicons/favicon-16x16.png" sizes="16x16'; ?>">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/favicons/favicon-32x32.png" sizes="32x32'; ?>">
-	<meta name="msapplication-TileColor" content="#b29e71">
-	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri() . '/favicons/mstile-144x144.png'; ?>">
-	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() . '/favicons/favicon.ico'; ?>">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon.png' ?>">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/favicons/favicon-32x32.png' ?>" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri() . '/favicons/favicon-16x16.png' ?>" sizes="16x16">
+	<link rel="manifest" href="<?php echo get_template_directory_uri() . '/favicons/manifest.json' ?>">
+	<link rel="mask-icon" href="<?php echo get_template_directory_uri() . '/favicons/safari-pinned-tab.svg' ?>" color="#ef4f4f">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() . '/favicons/favicon.ico' ?>">
+	<meta name="apple-mobile-web-app-title" content="LearnRuby.io">
+	<meta name="application-name" content="LearnRuby.io">
+	<meta name="msapplication-config" content="/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#ef4f4f">
 	
 	<link rel="manifest" href="/manifest.json">
 	<meta name="msapplication-TileColor" content="#b29e71">
