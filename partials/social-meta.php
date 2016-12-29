4534e70165a9394dc8645413849d1d2d8c9285d2
Zach Lawson <zachlawson.dev@gmail.com>
2016-12-29 16:19:30 -0700

    Ouput twitter meta tags

--- partials/social-meta.php
+++ partials/social-meta.php
@@ -1,8 +1,13 @@
 <!-- twitter meta -->
 <?php
 	$intro = "Learn the Ruby programming language for free, in a mobile friendly format." ;
+	$image_url = get_field('site_image');
+
 	if(get_field('intro')){
 		$intro = get_field('intro');
+	}
+	if(get_field('twitter_image_url')){
+		$image_url = get_field('twitter_image_url');
 	}
 ?>
 <meta name="twitter:card" content="summary_large_image">
@@ -12,6 +17,4 @@
 ?>
 <meta name="twitter:title" content="<?php echo get_the_title(); ?>">
 <meta name="twitter:description" content="<?php echo $intro; ?>">
-<?php if(get_field('twitter_image_url')): ?>
-	<meta name="twitter:image" content="<?php the_field('twitter_image_url'); ?>">
-<?php endif; ?>
+<meta name="twitter:image" content="<?php echo $image_url; ?>">
