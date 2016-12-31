5f46d0abb18d5af92e03e8d5682bbda14b5347f8
Zach Lawson <zachlawson.dev@gmail.com>
2016-12-31 12:27:30 -0700

    Fixed share image and added hotjar

--- header.php
+++ header.php
@@ -39,6 +39,17 @@
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
 	<?php wp_head(); ?>
+	<!-- Hotjar Tracking Code for www.learnruby.io -->
+	<script>
+	    (function(h,o,t,j,a,r){
+	        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+	        h._hjSettings={hjid:378672,hjsv:5};
+	        a=o.getElementsByTagName('head')[0];
+	        r=o.createElement('script');r.async=1;
+	        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+	        a.appendChild(r);
+	    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+	</script>
 </head>
 
 <body <?php body_class(); ?> id="body">
