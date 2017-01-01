330f2118d4f8ab594589840ba2607aa1bdc0d4b3
Zach Lawson <zachlawson.dev@gmail.com>
2017-01-01 15:04:35 -0700

    Changed the post.js to use jQuery rather $

--- source/js/post.js
+++ source/js/post.js
@@ -1,14 +1,13 @@
- (function($) {
- 	$( document ).ready(function() {
-	 	if($('#quiz').length > 0){
-			// $('#quiz').on("load", function() {
-			    $('.loader').remove();
-			// });
-	 	}
-	 	if($('.next').length > 0){
-	 		$(".next").mouseup(function() {
-	 			$(this).find('.heart').toggleClass("is-active");
-			});
-	 	}
- 	});
-})( jQuery );
+jQuery( document ).ready(function() {
+	if(jQuery('#quiz').length > 0){
+		jQuery('#quiz').on("load", function() {
+		    jQuery('.loader').remove();
+		});
+	}
+	if(jQuery('.next').length > 0){
+		jQuery(".next").mouseup(function() {
+			jQuery(this).find('.heart').toggleClass("is-active");
+		});
+		console.log('you got a next link');
+	}
+});
