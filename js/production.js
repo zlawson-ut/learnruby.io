8ed15ec187e84535653f0be374665efd109b9247
Zach Lawson <zachlawson.dev@gmail.com>
2017-01-01 15:08:52 -0700

    Debugging the iframe load event

--- js/production.js
+++ js/production.js
@@ -362,12 +362,12 @@ jQuery( document ).ready(function() {
 	if(jQuery('#quiz').length > 0){
 		jQuery('#quiz').on("load", function() {
 		    jQuery('.loader').remove();
+			console.log('quiz loaded!');
 		});
 	}
 	if(jQuery('.next').length > 0){
 		jQuery(".next").mouseup(function() {
 			jQuery(this).find('.heart').toggleClass("is-active");
 		});
-		console.log('you got a next link');
 	}
 });
