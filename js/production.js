c8408dcde227ef841f496c1e043a3dbea71daab1
Zach Lawson <zachlawson.dev@gmail.com>
2017-01-01 16:27:30 -0700

    Fixing click event'

--- js/production.js
+++ js/production.js
@@ -362,13 +362,15 @@ jQuery( document ).ready(function() {
 	if(jQuery('#quiz').length > 0){
 		jQuery('#quiz').ready(function() {
 		    jQuery('.loader').remove();
-			console.log('quiz loaded!');
 		});
-		console.log('You got a quiz!');
 	}
 	if(jQuery('.next').length > 0){
+		jQuery(".next").click(function(e) {
+			e.preventDefault();
+		});
 		jQuery(".next").mouseup(function() {
-			jQuery(this).find('.heart').toggleClass("is-active");
+			jQuery(this).find('.heart').addClass("is-active");
+			window.location.href = jQuery(this).attr('href');
 		});
 	}
 });
